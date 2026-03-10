e96e8e12efcc7090a091991a907c30576fb3ace2
Anmol <AmoCreates@outlook.com>
2026-03-10 19:36:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://virtual-assistant-kappa-two.vercel.app',
     credentials: true,
     methods: ["GET", "POST"]
 }));
