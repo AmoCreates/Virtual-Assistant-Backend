f3ae56fb958c09e9446362fa9b679c98a80f772f
Anmol <AmoCreates@outlook.com>
2026-03-10 20:52:49 +0530

    Update CORS origin for the application

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin: 'https://virtual-assistant-kappa-two.vercel.app',
+    origin: 'https://virtual-assistant-nu-pink.vercel.app',
     credentials: true,
     methods: ["GET", "POST"]
 }));
